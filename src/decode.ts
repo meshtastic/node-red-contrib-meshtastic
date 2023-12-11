e7324a2edb5f16fcfaf2642e0cc163efa333fcbc
Jason Murray <jason@chaosaffe.io>
2023-12-11 13:26:34 -0800

    lint: fix some biome warnings

--- src/decode.ts
+++ src/decode.ts
@@ -13,7 +13,7 @@ const nodeInit: NodeInitializer = (red): void => {
           return;
         }
 
-        var out;
+        let out: any;
 
         switch (decoded.packet.payloadVariant.case) {
           case "encrypted":
@@ -21,7 +21,7 @@ const nodeInit: NodeInitializer = (red): void => {
 
           case "decoded": {
             const { payload } = decoded.packet.payloadVariant.value;
-            const JsonWriteOptions = { "emitDefaultValues": true, "enumAsInteger": true };
+            const jsonWriteOptions = { emitDefaultValues: true, enumAsInteger: true };
             let data: unknown;
 
             switch (decoded.packet.payloadVariant.value.portnum) {
@@ -35,27 +35,27 @@ const nodeInit: NodeInitializer = (red): void => {
               }
 
               case Protobuf.PortNum.REMOTE_HARDWARE_APP: {
-                data = Protobuf.HardwareMessage.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.HardwareMessage.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
               case Protobuf.PortNum.POSITION_APP: {
-                data = Protobuf.Position.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.Position.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
               case Protobuf.PortNum.NODEINFO_APP: {
-                data = Protobuf.User.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.User.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
               case Protobuf.PortNum.ROUTING_APP: {
-                data = Protobuf.Routing.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.Routing.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
               case Protobuf.PortNum.ADMIN_APP: {
-                data = Protobuf.AdminMessage.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.AdminMessage.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
@@ -68,7 +68,7 @@ const nodeInit: NodeInitializer = (red): void => {
               }
 
               case Protobuf.PortNum.WAYPOINT_APP: {
-                data = Protobuf.Waypoint.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.Waypoint.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
@@ -94,7 +94,7 @@ const nodeInit: NodeInitializer = (red): void => {
               }
 
               case Protobuf.PortNum.STORE_FORWARD_APP: {
-                data = Protobuf.StoreAndForward.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.StoreAndForward.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
@@ -104,7 +104,7 @@ const nodeInit: NodeInitializer = (red): void => {
               }
 
               case Protobuf.PortNum.TELEMETRY_APP: {
-                data = Protobuf.Telemetry.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.Telemetry.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
@@ -121,7 +121,7 @@ const nodeInit: NodeInitializer = (red): void => {
               }
 
               case Protobuf.PortNum.NEIGHBORINFO_APP: {
-                data = Protobuf.NeighborInfo.fromBinary(payload).toJson(JsonWriteOptions);
+                data = Protobuf.NeighborInfo.fromBinary(payload).toJson(jsonWriteOptions);
                 break;
               }
 
@@ -136,16 +136,16 @@ const nodeInit: NodeInitializer = (red): void => {
 
             console.log(data);
 
-            out = decoded.toJson(JsonWriteOptions)
+            out = decoded.toJson(jsonWriteOptions)
 
             if (data) {
               out.packet.decoded.payload = data;
             }
 
             // include packet.payloadVariant for legacy flows
-            var payloadVariant = {
-              "oneofKind": "decoded",
-              "decoded": out.packet.decoded
+            const payloadVariant = {
+              oneofKind: "decoded",
+              decoded: out.packet.decoded
             };
             out.packet.payloadVariant = payloadVariant;
 
